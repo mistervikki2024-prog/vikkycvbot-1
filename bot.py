633f363d9de4a3f216e0102a1bca40cf1546f306
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-13 08:26:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -488,8 +488,5 @@ def run_bot():
     updater.start_polling()
     updater.idle()
 
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
 
-    port = int(os.environ.get("PORT", 10000))
-    web.run(host="0.0.0.0", port=port)
+threading.Thread(target=run_bot).start()
