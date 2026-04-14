2a60b9e91e46299f3623d15e408e53d698393ab5
vikkivivek201-prog <vikkivivek201@gmail.com>
2026-04-14 23:27:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "5328734113"))
 
 main_menu = [
-    ["Text to VCF,icon_custom_emoji_id="6069143072510318877", "📄 VCF to Text"],
+    ["Text to VCF,icon_custom_emoji_id="6069143072510318877", "VCF to Text,icon_custom_emoji_id="6069143072510318877"],
     ["📄 Manual VCF", "📁 Manual Text"],
     ["🔄 Merge VCF", "✂️ Split Text"],
     ["✍️ VCF Editer", "💳 My Subscription"],
